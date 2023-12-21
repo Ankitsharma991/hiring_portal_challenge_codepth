808e30ecc3840f9f1232133d879da9f9f34a614c
Ankit Sharma <sharmaa7861@gmail.com>
2023-12-21 18:21:11 +0530

    updated: folder structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { Fragment } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Landingpage from "./components/Landingpage";
-import Login from "./components/auth/Login";
-import Signup from "./components/auth/Signup";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Floatnav from "./components/Floatnav";
+import Homepage from "./components/Homepage";
+import LoginCard from "./components/auth/LoginCard";
+import SignupCard from "./components/auth/SignupCard";
 
 function App() {
   return (
     <Fragment>
       <Router>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Landingpage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<LoginCard />} />
+          <Route path="/signup" element={<SignupCard />} />
         </Routes>
+        <Floatnav />
+        <Footer />
       </Router>
     </Fragment>
   );
